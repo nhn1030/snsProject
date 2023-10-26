77bdb15b8a1058f22beef12bfae4f14c1b76a69f
nhn1030 <shgusska12@gmail.com>
2023-10-26 22:27:23 +0900

    first commit

---
+++ src/test/java/snsProject/SnsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package snsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
