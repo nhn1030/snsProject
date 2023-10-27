97f186bc263e94668f3512d551becffb53ee263e
nhn1030 <shgusska12@gmail.com>
2023-10-27 22:10:50 +0900

    feat: MVC와 템플릿엔진

--- src/main/java/snsProject/Controller/SnsController.java
+++ src/main/java/snsProject/Controller/SnsController.java
@@ -3,6 +3,7 @@ package snsProject.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SnsController {
@@ -10,5 +11,11 @@ public class SnsController {
     public String userController(Model model) {
         model.addAttribute("data", "환영합니다!");
         return "sns";
+    }
+
+    @GetMapping("sns-mvc")
+    public String snsMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "sns-template";
     }
 }
