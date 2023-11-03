398b329b968789399b29018565978a5c5a8950b7
nhn1030 <shgusska12@gmail.com>
2023-11-03 10:53:25 +0900

    HTTP GET 요청 처리

--- src/main/java/snsProject/Controller/SnsController.java
+++ src/main/java/snsProject/Controller/SnsController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SnsController {
@@ -17,5 +18,11 @@ public class SnsController {
     public String snsMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "sns-template";
+    }
+
+    @GetMapping("sns-string")
+    @ResponseBody
+    public String snsString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
